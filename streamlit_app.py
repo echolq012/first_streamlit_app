6fc52919defc44e8f2b315164533098c6adb30eb
Qing Li <44628521+echolq012@users.noreply.github.com>
2023-03-19 13:30:57 -0700

    Update streamlit_app.py
    re-structure

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,17 +27,20 @@ streamlit.dataframe(fruits_to_show)
 
 # new section to display fruityvice api response
 streamlit.header('Fruityvice Fruit Advice!')
-fruit_choice = streamlit.text_input('What fruit would you like information about', 'Kiwi')
-streamlit.write('The user entered ', fruit_choice)
+try:
+  fruit_choice = streamlit.text_input('What fruit would you like information about')
+  if not fruit_choice:
+      streamlit.error("Please select a fruit to get information.")
+  else:
+      fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+      #Normalize semi-structured JSON data into a flat table and return dataframe
+      fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
+      # display it as a table in streamlit app
+      streamlit.dataframe(fruityvice_normalized)
 
+except URLError as e:
+    streamlit.error()
 
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-#streamlit.text(fruityvice_response.json()) # just writes the data to the screen
-
-#Normalize semi-structured JSON data into a flat table and return dataframe
-fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
-# display it as a table in streamlit app
-streamlit.dataframe(fruityvice_normalized)
 #don't run anything past here while we troubleshoot
 streamlit.stop()
 
